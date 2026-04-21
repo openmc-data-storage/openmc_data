1f808a6f51f5641865ca4931e580518c26c5fdce
shimwell <mail@jshimwell.com>
2026-04-21 12:01:14 +0200

    adding download endf

--- src/openmc_data/download/download_endf.py
+++ src/openmc_data/download/download_endf.py
@@ -44,10 +44,10 @@ parser.add_argument(
 parser.add_argument(
     "-r",
     "--release",
-    choices=["b7.1", "b8.0"],
-    default="b7.1",
+    choices=["b7.1", "b8.0", "b8.1"],
+    default="b8.1",
     help="The nuclear data library release version. The currently supported "
-         "options are 'b7.1' and 'b8.0'",
+         "options are 'b7.1', 'b8.0' and 'b8.1'",
 )
 parser.add_argument(
     "--cleanup",
